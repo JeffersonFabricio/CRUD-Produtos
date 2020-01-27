69dbac42a5977d31f1bb8b3c4d46451fe9b2fce9
Jefferson Fabrício <jeffersonfabricioma@gmail.com>
2020-01-26 22:40:20 -0300

    layout e tela inicial

--- client/src/app/order/order.ts
+++ client/src/app/order/order.ts
@@ -2,7 +2,6 @@ import { Product } from '../product/product';
 
 export interface Order {
   stock: number;
-  total: number;
   products: Product[];
   //user: User;
   _id ?: string;
--- client/src/app/product/component/product.component.ts
+++ client/src/app/product/component/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  productName = '';
+  productDescription = '';
+  productPrice: number;
+  productUnit = '';
+  productLinkProduct = '';
+  productLinkImg = '';
+  products: Product[] = [];
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  save(){
+    //to do
+  }
+
+  edit(prod: Product){
+    //to do
+  }
+
+  delete(prod: Product){
+    //to do
+  }
+
+  cancel(){
+    //to do
+  }
+
+
 
 }
