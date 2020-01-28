7ddbf4c980adcf2c5f3d7406becf514cc243adbd
Jefferson Fabrício <jeffersonfabricioma@gmail.com>
2020-01-27 22:02:43 -0300

    add product

--- client/src/app/product/service/product.service.ts
+++ client/src/app/product/service/product.service.ts
@@ -1,7 +1,8 @@
 import { Product } from './../product';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,26 @@ export class ProductService {
 
   readonly url = 'http://localhost:3000/products';
 
+  private productsSubject$: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>(null);
+  private loaded = false;
+
   constructor(private http: HttpClient) { }
 
   get(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.url);
+    if (!this.loaded) {
+      this.http.get<Product[]>(this.url)
+        .pipe(tap((prods) => console.log(prods)))
+        .subscribe(this.productsSubject$);
+      this.loaded = true;
+    }
+    return this.productsSubject$.asObservable();
   }
 
   add(prod: Product): Observable<Product> {
-    return this.http.post<Product>(this.url, prod);
+    return this.http.post<Product>(this.url, prod)
+    .pipe(
+      tap((prods: Product) => this.productsSubject$.getValue().push(prods))
+    );
   }
 
 }
